d4eabb9a1bd2d71a3b252a2de33a74abae74d3aa
CarmaClisthert <mariela.santos.ptech@gmail.com>
2024-11-16 13:31:09 -0500

    Modified PlayerMovement and Physics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,33 +3,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
+   
     private float horizontal;
-    public float jump = 16f;
-    private bool isFacingRight = true;
+    public float jumpingPower = 16f;
     public float speed = 8f; // Speed of horizontal movement
+    private bool isFacingRight = true;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
         // Handle jumping
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.linearVelocity.y) < 0.01f) // Ensure the player can only jump if nearly grounded
+        if (Input.GetButtonDown("Jump") && IsGround()) // Ensure the player can only jump if nearly grounded
         {
-            rb.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
 
-        // Handle horizontal movement
-        float moveInput = Input.GetAxis("Horizontal"); // Get input for horizontal movement (A/D or Left/Right keys)
-        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        flip();
 
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private bool IsGround()
+    {
+        return Physics2D.OverlapCircle(groundChecker.position, 0.2f, groundLayer);
     }
 
     private void flip()
