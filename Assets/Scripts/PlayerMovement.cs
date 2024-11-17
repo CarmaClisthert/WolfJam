fd3b7166322f1aed3afa806ea0faa7138a956f53
lakota56541 <eckmal2@rpi.edu>
2024-11-16 22:26:45 -0500

    Trying UI Stuff
    sad

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
@@ -26,7 +27,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
-        
+        if (Input.GetKey(KeyCode.L) || (Input.GetKey(KeyCode.F)))
+        {
+            CutScene();
+        }
 
         horizontal = Input.GetAxisRaw("Horizontal");
 
@@ -86,5 +90,10 @@ public class PlayerMovement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    private void CutScene()
+    {
+        var gooncontainer = GameObject.Find("UIPrefab");
     }
 }
