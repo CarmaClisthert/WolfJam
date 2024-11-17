2867f840d35dfba1fe7d3c7d4da9c9930f520644
Jared Babinec <jbabinec1@gmail.com>
2024-11-16 20:25:48 -0500

    add dynamic npc generation

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,6 +8,7 @@ public class PlatformSpawner : MonoBehaviour
     public float minX = -5f, maxX = 5f;
 
     private float nextSpawnY = 0f;
+    public NPCSpawner npcSpawner;
 
     void Update()
     {
@@ -25,7 +26,34 @@ public class PlatformSpawner : MonoBehaviour
             0
         );
 
+        GameObject spawnedPlatform = Instantiate(
+            platformPrefabs[Random.Range(0, platformPrefabs.Length)],
+            spawnPosition,
+            Quaternion.identity
+        );
+
+        // Force NPC spawn for testing
+        if (npcSpawner != null)
+        {
+            npcSpawner.SpawnNPC(spawnedPlatform);
+        }
+
+        nextSpawnY += Random.Range(2f, 4f);
+    }
+
+
+
+
+    /* OG DO NOT DELETE
+    void SpawnPlatform()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(minX, maxX),
+            nextSpawnY,
+            0
+        );
+
         Instantiate(platformPrefabs[Random.Range(0, platformPrefabs.Length)], spawnPosition, Quaternion.identity);
         nextSpawnY += Random.Range(2f, 4f); // Adjust gap between platforms
-    }
+    } */
 }
