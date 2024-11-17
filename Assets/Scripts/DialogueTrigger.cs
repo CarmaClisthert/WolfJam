1f2b13ab92359fbdc182e67c436688b492337a50
Jared Babinec <jbabinec1@gmail.com>
2024-11-16 20:49:50 -0500

    ?

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,28 @@
+
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+  //  public Dialogue dialogue;
+
+   // public GameObject dialogueBox;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"Object entered trigger: {other.gameObject.name}");
+
+
+
+      //  FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+      //  dialogueBox.SetActive(false);
+    }
+
+
+
+}
+
