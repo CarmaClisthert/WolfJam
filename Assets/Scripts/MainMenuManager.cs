ae67891e52d5f0c01e091a177f3b83d757dc12b1
PixelTaku <duperx678@gmail.com>
2024-11-16 22:00:48 -0500

    added main menu functionality

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,7 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private Image background;
+
+    [SerializeField]
+    private Sprite howToPlay;
+    [SerializeField]
+    private GameObject[] buttons;
+    // start, quit, start fr
+
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +25,24 @@ public class MainMenuManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MenuManager(int i)
+    {
+        switch (i)
+        {
+            case 0://start
+                background.sprite = howToPlay;
+                buttons[0].SetActive(false);
+                buttons[1].SetActive(false);
+                buttons[2].SetActive(true);
+                break;
+            case 1://quit
+                Application.Quit();
+                break;
+            case 2://start fr
+                SceneManager.LoadScene(1);
+                break;
+        }
     }
 }
