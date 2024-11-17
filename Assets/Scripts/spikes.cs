70ce237164b9c8a0780280a6d65c430e8dcdf8a6
CarmaClisthert <mariela.santos.ptech@gmail.com>
2024-11-16 21:42:02 -0500

    adding assets

---
+++ Assets/Scripts/spikes.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class spikes : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.collider.GetComponent<Player>())
+        {
+            //collision.collider.GetComponent<Collider2D>
+        }
+    }
+
+}
